0d5d92dae0b08755a093e789ac094b0e37bd4de1
Vittorio Meloni <vitto.meloni@gmail.com>
2020-04-09 16:09:47 +0200

    Adds constants for modifiers

--- fhirserver/parser_types.py
+++ fhirserver/parser_types.py
@@ -1,22 +1,66 @@
+import importlib
 from enum import Enum
 
 from dateutil.parser import isoparse
 from flask_restful.reqparse import Argument
+# from flask import current_app
+#
+# operators_mod = importlib.import_module(current_app.config['OPERATORS_MODULE'])
 
 _prefixes = ('eq', 'ne', 'gt', 'lt', 'ge', 'le', 'gt', 'sa', 'eb', 'ap')
+
+
+class FHIRModifiers(object):
+    MISSING = ':missing'
+    EXACT = ':exact'
+    CONTAINS = ':contains'
+    TEXT = ':text'
+    NOT = ':not'
+    IN = ':in'
+    NOT_IN = ':not-in'
+    BELOW = ':below'
+    ABOVE = ':above'
+    IDENTIFIER = ':identifier'
+
+
+class FHIRSearchTypes(Enum):
+    NUMBER = 1
+    DATE = 2
+    STRING = 3
+    TOKEN = 4
+    REFERENCE = 5
+    QUANTITY = 6
+    URI = 7
+
+    @classmethod
+    def has_value(cls, value):
+        return value in cls._value2member_map_
+
+    @classmethod
+    def get_type_handler(cls, enum_value):
+        typ_map = {
+            cls.NUMBER: FHIRNumber,
+            cls.DATE: FHIRDate,
+            cls.STRING: FHIRString,
+            cls.TOKEN: FHIRToken,
+            cls.REFERENCE: FHIRReference,
+            cls.QUANTITY: FHIRQuantity,
+            cls.URI: FHIRUri
+        }
+        return typ_map[enum_value]
 
 
 def _check_operator(value, operator, allowed_modifiers):
     if operator not in allowed_modifiers and operator is not None:
         raise ValueError
 
-    if operator == ':missing=' and value not in ('true', 'false'):
+    if operator == FHIRModifiers.MISSING and value not in ('true', 'false'):
         raise ValueError
 
 
 class BaseFHIRSearch(object):
 
-    OPERATORS = ('=', ':missing=')
+    OPERATORS = ('=', FHIRModifiers.MISSING)
 
     def __init__(self, value, name=None, operator='='):
 
@@ -24,7 +68,7 @@ class BaseFHIRSearch(object):
 
         self.modifier = operator.replace('=', '') if operator != '=' and operator is not None else None
 
-        if self.modifier == ':missing':
+        if self.modifier == FHIRModifiers.MISSING:
             self.value = value == 'true'
         else:
             self.value = None
@@ -76,17 +120,33 @@ class FHIRDate(BaseFHIRSearch):
 
 class FHIRString(BaseFHIRSearch):
 
-    OPERATORS = ('=', ':missing=', ':exact=', ':contains=')
+    OPERATORS = ('=', FHIRModifiers.MISSING, FHIRModifiers.EXACT, FHIRModifiers.CONTAINS)
 
     def __init__(self, value, name=None, operator='='):
         super(FHIRString, self).__init__(value, name, operator)
         if not isinstance(self.value, bool):
             self.value = value
 
+        # if self.modifier is None:
+        #     self.operator = operators_mod.eq
+        # elif self.modifier == FHIRModifiers.EXACT:
+        #     self.operator = operators_mod.eq
+        # elif self.modifier == FHIRModifiers.MISSING:
+        #     self.operator = operators_mod.missing
+        # elif self.modifier:
+        #     self.operator = operators_mod.contains
+        #     if qp.value is True:
+        #         filters.append(getattr(PatientModel, name) == None)
+        #     else:
+        #         filters.append(getattr(PatientModel, name) != None)
+        # elif self.modifier == FHIRModifiers.CONTAINS:
+        #     filters.append(getattr(PatientModel, name).like('%{}%'.format(qp.value)))
+
 
 class FHIRToken(BaseFHIRSearch):
 
-    OPERATORS = ('=', ':missing=', ':not=', ':in=', ':not-in=', ':below=', ':above=')
+    OPERATORS = ('=', FHIRModifiers.MISSING, FHIRModifiers.TEXT, FHIRModifiers.NOT,
+                 FHIRModifiers.IN, FHIRModifiers.NOT_IN, FHIRModifiers.BELOW, FHIRModifiers.ABOVE)
 
     def __init__(self, value, name=None, operator='='):
         super(FHIRToken, self).__init__(value, name, operator)
@@ -109,7 +169,7 @@ class FHIRToken(BaseFHIRSearch):
 
 class FHIRReference(BaseFHIRSearch):
 
-    OPERATORS = ['=', ':missing=', ':identifier=', ':above=', ':below=']
+    OPERATORS = ['=', FHIRModifiers.MISSING, FHIRModifiers.IDENTIFIER, FHIRModifiers.ABOVE, FHIRModifiers.BELOW]
 
     def __init__(self, value, name=None, operator='='):
         # we reduce case like Observation?subject:Patient=23 to Observation?subject=Patient/23
@@ -157,7 +217,7 @@ class FHIRQuantity(FHIRNumber):
 
 
 class FHIRUri(BaseFHIRSearch):
-    OPERATORS = ('=', ':missing=', ':above=', ':below=')
+    OPERATORS = ('=', FHIRModifiers.MISSING, FHIRModifiers.ABOVE, FHIRModifiers.BELOW)
 
     def __init__(self, value, name=None, operator='='):
         super(FHIRUri, self).__init__(value, name, operator)
@@ -166,38 +226,11 @@ class FHIRUri(BaseFHIRSearch):
             self.value = value
 
 
-class FHIRSearchTypes(Enum):
-    NUMBER = 1
-    DATE = 2
-    STRING = 3
-    TOKEN = 4
-    REFERENCE = 5
-    QUANTITY = 6
-    URI = 7
-
-    @classmethod
-    def has_value(cls, value):
-        return value in cls._value2member_map_
-
-    @classmethod
-    def get_type_handler(cls, enum_value):
-        typ_map = {
-            cls.NUMBER: FHIRNumber,
-            cls.DATE: FHIRDate,
-            cls.STRING: FHIRString,
-            cls.TOKEN: FHIRToken,
-            cls.REFERENCE: FHIRReference,
-            cls.QUANTITY: FHIRQuantity,
-            cls.URI: FHIRUri
-        }
-        return typ_map[enum_value]
-
-
 def query_argument_type_factory(name, typ, dest=None):
     try:
         typ_handler = FHIRSearchTypes.get_type_handler(typ)
     except KeyError:
         raise Exception('Unkown argument type')
-
+    print(typ_handler.OPERATORS)
     return Argument(name, dest=dest, type=typ_handler, operators=typ_handler.OPERATORS,
                     required=False, location='args')
--- test/test_parser_type.py
+++ test/test_parser_type.py
@@ -2,7 +2,8 @@ from unittest import TestCase
 
 from dateutil.parser import isoparse
 
-from fhirserver.parser_types import FHIRNumber, FHIRDate, FHIRString, FHIRToken, FHIRReference, FHIRQuantity, FHIRUri
+from fhirserver.parser_types import FHIRNumber, FHIRDate, FHIRString, FHIRToken, FHIRReference, FHIRQuantity, FHIRUri, \
+    FHIRModifiers
 from fhirserver.resources import RESOURCES
 
 _search_prefixes = ('eq', 'ne', 'gt', 'lt', 'ge', 'le', 'gt', 'sa', 'eb', 'ap')
@@ -17,15 +18,15 @@ class TestFHIRSearchTypes(TestCase):
         self.assertIsNone(fn.modifier)
 
     def test_number_with_missing_modifier(self):
-        fn = FHIRNumber('true', None, ':missing=')
+        fn = FHIRNumber('true', None, FHIRModifiers.MISSING)
         self.assertTrue(fn.value)
         self.assertIsNone(fn.operation)
-        self.assertEqual(fn.modifier, ':missing')
+        self.assertEqual(fn.modifier, FHIRModifiers.MISSING)
 
-        fn = FHIRNumber('false', None, ':missing=')
+        fn = FHIRNumber('false', None, FHIRModifiers.MISSING)
         self.assertFalse(fn.value)
         self.assertIsNone(fn.operation)
-        self.assertEqual(fn.modifier, ':missing')
+        self.assertEqual(fn.modifier, FHIRModifiers.MISSING)
 
     def test_number_with_float_string(self):
         fn = FHIRNumber("100.00")
@@ -54,7 +55,7 @@ class TestFHIRSearchTypes(TestCase):
         self.assertRaises(ValueError, FHIRNumber, "st100", None, None)
         self.assertRaises(ValueError, FHIRNumber, "100,00", None, None)
         self.assertRaises(ValueError, FHIRNumber, "notanumber", None, None)
-        self.assertRaises(ValueError, FHIRNumber, '100', None, ':missing=')
+        self.assertRaises(ValueError, FHIRNumber, '100', None, FHIRModifiers.MISSING)
 
     def test_date(self):
         for op in _search_prefixes:
@@ -65,15 +66,15 @@ class TestFHIRSearchTypes(TestCase):
                 self.assertEqual(fd.value, isoparse(date_str))
 
     def test_date_with_missing_modifier(self):
-        fd = FHIRDate('true', None, ':missing=')
+        fd = FHIRDate('true', None, FHIRModifiers.MISSING)
         self.assertTrue(fd.value)
         self.assertIsNone(fd.operation)
-        self.assertEqual(fd.modifier, ':missing')
+        self.assertEqual(fd.modifier, FHIRModifiers.MISSING)
 
-        fd = FHIRDate('false', None, ':missing=')
+        fd = FHIRDate('false', None, FHIRModifiers.MISSING)
         self.assertFalse(fd.value)
         self.assertIsNone(fd.operation)
-        self.assertEqual(fd.modifier, ':missing')
+        self.assertEqual(fd.modifier, FHIRModifiers.MISSING)
 
     def test_wrong_date(self):
         # wrong date values
@@ -89,19 +90,19 @@ class TestFHIRSearchTypes(TestCase):
 
     def test_string(self):
         test_value = "testvalue"
-        for operator in (':exact=', ':contains='):
+        for operator in (FHIRModifiers.EXACT, FHIRModifiers.CONTAINS):
             fs = FHIRString(test_value, None, operator)
             self.assertEqual(fs.value, test_value)
-            self.assertEqual(fs.modifier, operator.replace('=', ''))
+            self.assertEqual(fs.modifier, operator)
 
     def test_string_with_missing_modifier(self):
-        fs = FHIRString('true', None, ':missing=')
+        fs = FHIRString('true', None, FHIRModifiers.MISSING)
         self.assertTrue(fs.value)
-        self.assertEqual(fs.modifier, ':missing')
+        self.assertEqual(fs.modifier, FHIRModifiers.MISSING)
 
-        fs = FHIRString('false', None, ':missing=')
+        fs = FHIRString('false', None, FHIRModifiers.MISSING)
         self.assertFalse(fs.value)
-        self.assertEqual(fs.modifier, ':missing')
+        self.assertEqual(fs.modifier, FHIRModifiers.MISSING)
 
     def test_wrong_string(self):
         """
@@ -112,47 +113,47 @@ class TestFHIRSearchTypes(TestCase):
 
     def test_token(self):
         test_value = "testvalue"
-        for operator in (':not=', ':in=', ':not-in=', ':below=', ':above='):
+        for operator in (FHIRModifiers.TEXT, FHIRModifiers.IN, FHIRModifiers.NOT_IN, ':below', FHIRModifiers.ABOVE):
             ft = FHIRToken(test_value, None, operator)
             self.assertEqual(ft.system, '')
             self.assertEqual(ft.value, test_value)
-            self.assertEqual(ft.modifier, operator.replace('=', ''))
+            self.assertEqual(ft.modifier, operator)
 
     def test_token_with_code_system(self):
         system_value = 'system_code'
         code_value = 'testvalue'
-        for operator in (':not=', ':in=', ':not-in=', ':below=', ':above='):
+        for operator in (FHIRModifiers.TEXT, FHIRModifiers.IN, FHIRModifiers.NOT_IN, FHIRModifiers.BELOW, FHIRModifiers.ABOVE):
             ft = FHIRToken('{}|{}'.format(system_value, code_value), None, operator)
             self.assertEqual(ft.system, system_value)
             self.assertEqual(ft.value, code_value)
-            self.assertEqual(ft.modifier, operator.replace('=', ''))
+            self.assertEqual(ft.modifier, operator)
 
     def test_token_with_missing_modifier(self):
-        ft = FHIRToken('true', None, ':missing=')
+        ft = FHIRToken('true', None, FHIRModifiers.MISSING)
         self.assertTrue(ft.value)
         self.assertIsNone(ft.system)
-        self.assertEqual(ft.modifier, ':missing')
+        self.assertEqual(ft.modifier, FHIRModifiers.MISSING)
 
-        ft = FHIRToken('false', None, ':missing=')
+        ft = FHIRToken('false', None, FHIRModifiers.MISSING)
         self.assertFalse(ft.value)
         self.assertIsNone(ft.system)
-        self.assertEqual(ft.modifier, ':missing')
+        self.assertEqual(ft.modifier, FHIRModifiers.MISSING)
 
     def test_wrong_token(self):
         test_value = 'testvalue'
         self.assertRaises(ValueError, FHIRString, test_value, None, 'unknown')
 
         test_value = 'system_code|testvalue|unkown_part'
-        self.assertRaises(ValueError, FHIRToken, test_value, None, ':missing=')
+        self.assertRaises(ValueError, FHIRToken, test_value, None, FHIRModifiers.MISSING)
 
     def test_reference(self):
         test_value = '12'  # logical id
 
-        for operator in (':identifier=', ':above=', ':below='):
+        for operator in (FHIRModifiers.IDENTIFIER, FHIRModifiers.ABOVE, FHIRModifiers.BELOW):
             fr = FHIRReference(test_value, None, operator)
             self.assertEqual(fr.type, None)
             self.assertEqual(fr.value, test_value)
-            self.assertEqual(fr.modifier, operator.replace('=', ''))
+            self.assertEqual(fr.modifier, operator)
 
         for r in RESOURCES:
             fr = FHIRReference('{}/{}'.format(r, test_value), None, None)
@@ -160,22 +161,22 @@ class TestFHIRSearchTypes(TestCase):
             self.assertEqual(fr.value, test_value)
             self.assertEqual(fr.modifier, None)
 
-            for operator in (':identifier=', ':above=', ':below='):
+            for operator in (FHIRModifiers.IDENTIFIER, FHIRModifiers.ABOVE, FHIRModifiers.BELOW):
                 fr = FHIRReference('{}/{}'.format(r, test_value), None, operator)
                 self.assertEqual(fr.type, r)
                 self.assertEqual(fr.value, test_value)
-                self.assertEqual(fr.modifier, operator.replace('=', ''))
+                self.assertEqual(fr.modifier, operator)
 
     def test_reference_with_missing_modifier(self):
-        fr = FHIRReference('true', None, ':missing=')
+        fr = FHIRReference('true', None, FHIRModifiers.MISSING)
         self.assertTrue(fr.value)
         self.assertIsNone(fr.type)
-        self.assertEqual(fr.modifier, ':missing')
+        self.assertEqual(fr.modifier, FHIRModifiers.MISSING)
 
-        fr = FHIRReference('false', None, ':missing=')
+        fr = FHIRReference('false', None, FHIRModifiers.MISSING)
         self.assertFalse(fr.value)
         self.assertIsNone(fr.type)
-        self.assertEqual(fr.modifier, ':missing')
+        self.assertEqual(fr.modifier, FHIRModifiers.MISSING)
 
     def test_wrong_reference(self):
         self.assertRaises(ValueError, FHIRReference, 'Patient/12', None, 'Patient')
@@ -200,35 +201,35 @@ class TestFHIRSearchTypes(TestCase):
             self.assertIsNone(fq.modifier)
 
     def test_quantity_with_missing_modifier(self):
-        fq = FHIRQuantity('true', None, ':missing=')
+        fq = FHIRQuantity('true', None, FHIRModifiers.MISSING)
         self.assertTrue(fq.value)
         self.assertIsNone(fq.operation)
         self.assertIsNone(fq.system)
         self.assertIsNone(fq.code)
-        self.assertEqual(fq.modifier, ':missing')
+        self.assertEqual(fq.modifier, FHIRModifiers.MISSING)
 
-        fq = FHIRQuantity('false', None, ':missing=')
+        fq = FHIRQuantity('false', None, FHIRModifiers.MISSING)
         self.assertFalse(fq.value)
         self.assertIsNone(fq.operation)
         self.assertIsNone(fq.system)
         self.assertIsNone(fq.code)
-        self.assertEqual(fq.modifier, ':missing')
+        self.assertEqual(fq.modifier, FHIRModifiers.MISSING)
 
     def test_quantity_error(self):
         self.assertRaises(ValueError, FHIRQuantity, '5.4|http://unitsofmeasure.org')
 
     def test_uri(self):
         value = 'http://acme.org/fhir/ValueSet/123'
-        for operator in (':above=', ':below='):
+        for operator in (FHIRModifiers.ABOVE, FHIRModifiers.BELOW):
             fu = FHIRUri(value, None, operator)
             self.assertEqual(fu.value, value)
-            self.assertEqual(fu.modifier, operator.replace('=', ''))
+            self.assertEqual(fu.modifier, operator)
 
     def test_uri_with_missing_modifier(self):
-        fu = FHIRUri('true', None, ':missing=')
+        fu = FHIRUri('true', None, FHIRModifiers.MISSING)
         self.assertTrue(fu.value)
-        self.assertEqual(fu.modifier, ':missing')
+        self.assertEqual(fu.modifier, FHIRModifiers.MISSING)
 
-        fu = FHIRUri('false', None, ':missing=')
+        fu = FHIRUri('false', None, FHIRModifiers.MISSING)
         self.assertFalse(fu.value)
-        self.assertEqual(fu.modifier, ':missing')
+        self.assertEqual(fu.modifier, FHIRModifiers.MISSING)
